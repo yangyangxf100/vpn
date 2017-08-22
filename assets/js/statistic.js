befe621375a599fce030ad0fbaf674253eac2349
yangyangxf100@gmail.com <yangyangxf1002@>
2017-08-22 10:37:29 +0800

    init

---
+++ assets/js/statistic.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?154c9544b3ae1f0823241fb40319ea60";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
